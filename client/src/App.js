b72c9699c9a6c0a3807f1aef1b4d1e11b8753c1d
AG <champtoday@gmail.com>
2021-02-11 12:16:49 -0500

    update app to functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,80 @@
-import React, { Component } from "react"
-import AmericaContract from "./contracts/America.json"
-import getWeb3 from "./getWeb3"
+import React, { Component, useEffect, useState } from "react";
+import AmericaContract from "./contracts/America.json";
+import getWeb3 from "./getWeb3";
 
-import "./App.css"
+import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null }
+const App = () => {
+  const [storageValue, setStorageValue] = useState(0);
+  const [web3, setWeb3] = useState(null);
 
-  componentDidMount = async () => {
+  useEffect(() => {
+    start();
+  }, []);
+
+  const start = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3()
+      const web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts()
+      const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId()
-      const deployedNetwork = AmericaContract.networks[networkId]
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = AmericaContract.networks[networkId];
       const instance = new web3.eth.Contract(
         AmericaContract.abi,
         deployedNetwork && deployedNetwork.address
-      )
+      );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance })
+      this.setState({ web3, accounts, contract: instance });
 
-      console.log(accounts[0])
+      console.log(accounts[0]);
 
-      console.log(await instance.methods.balanceOf(accounts[0], 1).call())
+      console.log(await instance.methods.balanceOf(accounts[0], 1).call());
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`
-      )
-      console.error(error)
+      );
+      console.error(error);
     }
-  }
+  };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state
+  const runExample = async () => {
+    const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] })
+    await contract.methods.set(5).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call()
+    const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response })
-  }
+    this.setState({ storageValue: response });
+  };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    )
+  if (!this.state.web3) {
+    return <div>Loading Web3, accounts, and contract...</div>;
   }
-}
+  return (
+    <div className="App">
+      <h1>Good to Go!</h1>
+      <p>Your Truffle Box is installed and ready.</p>
+      <h2>Smart Contract Example</h2>
+      <p>
+        If your contracts compiled and migrated successfully, below will show a
+        stored value of 5 (by default).
+      </p>
+      <p>
+        Try changing the value stored on <strong>line 40</strong> of App.js.
+      </p>
+      <div>The stored value is: {this.state.storageValue}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
