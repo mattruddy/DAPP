e9fc73cd76b6f8e89f78dbfaeb9516d6e5998eaa
Matt <mjruddy94@gmail.com>
2021-02-16 13:35:38 -0500

    add pixel

--- client/src/App.js
+++ client/src/App.js
@@ -1,90 +1,91 @@
-import React, { Component, useEffect, useState } from "react";
-import PixelToken from "./contracts/PixelToken.json";
-import getWeb3 from "./getWeb3";
+import React, { Component, useEffect, useState } from "react"
+import PixelToken from "./contracts/PixelToken.json"
+import getWeb3 from "./getWeb3"
 
-import "./App.css";
+import "./App.css"
 
 const App = () => {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts] = useState();
-  const [contract, setContract] = useState();
+  const [web3, setWeb3] = useState(null)
+  const [accounts, setAccounts] = useState()
+  const [contract, setContract] = useState()
+  const [pixels, setPixels] = useState([])
 
   useEffect(() => {
-    start();
-  }, []);
+    start()
+  }, [])
 
   const start = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      const web3 = await getWeb3()
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      const accounts = await web3.eth.getAccounts()
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = PixelToken.networks[networkId];
+      const networkId = await web3.eth.net.getId()
+      const deployedNetwork = PixelToken.networks[networkId]
       const instance = new web3.eth.Contract(
         PixelToken.abi,
         deployedNetwork && deployedNetwork.address
-      );
+      )
 
-      setWeb3(web3);
-      setAccounts(accounts);
-      setContract(instance);
-
-      console.log(accounts[0]);
-
-      console.log(await instance.methods.balanceOf(accounts[0], 1).call());
+      setWeb3(web3)
+      setAccounts(accounts)
+      setContract(instance)
+      fetchPixels(instance)
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
+      )
+      console.error(error)
     }
-  };
+  }
+
+  const fetchPixels = async (instance) => {
+    const p = await instance.methods.getAllPixels().call()
+    console.log(p)
+    setPixels(p)
+  }
 
   const handleCreate = async (e) => {
-    e.preventDefault();
-
-    console.log(contract);
-
+    e.preventDefault()
     if (contract) {
-      console.log(
-        await contract.methods.create(accounts[0], 10000000000).send({
-          from: accounts[0],
-        })
-      );
+      await contract.methods.create("Yo").send({
+        from: accounts[0],
+      })
+      fetchPixels(contract)
     }
-  };
+  }
 
   const handleSend = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     contract.methods
       .safeTransferFrom(
         accounts[0],
         "0x81431b69B1e0E334d4161A13C2955e0f3599381e",
         1,
-        10000,
+        1,
         Buffer.from("test")
       )
       .send({
         from: accounts[0],
-      });
-  };
+      })
+  }
 
   if (!web3) {
-    return <div>Loading Web3, accounts, and contract...</div>;
+    return <div>Loading Web3, accounts, and contract...</div>
   }
   return (
     <div className="App">
       <button onClick={(e) => handleCreate(e)}>Create Token</button>
 
       <button onClick={(e) => handleSend(e)}>Send</button>
+      <ul>{pixels && pixels.map((pixel, i) => <li key={i}>{pixel[1]}</li>)}</ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- contracts/PixelToken.sol
+++ contracts/PixelToken.sol
@@ -1,15 +1,23 @@
 pragma solidity 0.6.2;
+pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract PixelToken is ERC1155 {
 
+    struct Pixel {
+        uint256 id;
+        Meta meta;
+    }
+
+    struct Meta {
+        address account;
+        string name;
+    }
+
     address proxyRegistryAddress;
     uint256 private _currentTokenID = 0;
-    mapping (uint256 => address) public creators;
-    mapping (uint256 => uint256) public tokenSupply;
-
-
+    mapping (uint256 => Meta) public creators;
 
     // Contract name
     string public name;
@@ -17,29 +25,50 @@ contract PixelToken is ERC1155 {
     string public symbol;
 
     modifier creatorOnly(uint256 _id) {
-        require(creators[_id] == msg.sender, "Creator Only");
+        require(creators[_id].account == msg.sender, "Creator Only");
         _;
     }
 
-    constructor() public ERC1155("https://github.com") {
+    constructor() public ERC1155("https://github.com/mattruddy/DAPP/blob/master/item/{id}.json") {
         name = "PixelToken";
         symbol = "PXT";
         proxyRegistryAddress = msg.sender;
 
         // create 100 pixel items
-        for (uint i=0; i<100; i++) {
-            _mint('0x16Fb96a5fa0427Af0C8F7cF1eB4870231c8154B6', i, 1, "");
+        for (uint i=0; i<10; i++) {
+            _mint(msg.sender, i, 1, "");
+            Meta memory meta = Meta({
+                account: msg.sender,
+                name: "Test Name"
+            });
+            creators[i] = meta;
+            _incrementTokenTypeId();
         }
     }
 
-    function create(address _initialOwner, uint256 _initialSupply) external returns(uint256) {
+    function create(string calldata itemName) external returns(uint256) {
         uint256 _id = _getNextTokenID();
         _incrementTokenTypeId();
-        creators[_id] = msg.sender;
 
-        _mint(_initialOwner, _id, _initialSupply, "");
-        tokenSupply[_id] = _initialSupply;
+        Meta memory meta = Meta({
+            account: msg.sender,
+            name: itemName
+        });
+        creators[_id] = meta;
+
+        _mint(msg.sender, _id, 1, "");
         return _id;
+    }
+
+    function getAllPixels() public view returns(Pixel[] memory) {
+        Pixel[] memory pixels = new Pixel[](_currentTokenID);
+        for(uint i =0; i < _currentTokenID; i++) {
+            pixels[i] = Pixel({
+                id: i,
+                meta: creators[i]
+            });
+        }
+        return pixels;
     }
 
     function _getNextTokenID() private view returns (uint256) {
