c611e1d0eeb4823714e6fe0f10adc7315f0684a6
Matt <mjruddy94@gmail.com>
2021-03-15 21:00:12 -0400

    update contract

--- contracts/PixelToken.sol
+++ contracts/PixelToken.sol
@@ -4,17 +4,14 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract PixelToken is ERC721 {
-
-    struct Exhibit {
-        uint256 id;
-        address payable owner;
+    struct Bounds {
+        Coord topLeft;
+        Coord bottomRight;
     }
 
-    struct Pixel {
-        bytes32 id;
-        string hexColor;
-        uint256 x;
-        uint256 y;
+    struct Coord {
+        uint16 x;
+        uint16 y;
     }
 
     struct Bid {
@@ -23,42 +20,37 @@ contract PixelToken is ERC721 {
     }
 
     struct HighestBidResponse {
-        uint256 exhibitId;
+        uint16 exhibitId;
         address fromAddress;
         uint256 amount;
     }
 
-    struct PixelResponse {
-        uint256 exhibitId;
+    struct ExhibitResp {
+        uint16 exhibitId;
         address owner;
-        bytes32 pixelId;
-        string hexColor;
-        uint256 x;
-        uint256 y;
+        uint8[] rgbArray;
+        Bounds bounds;
     }
 
     address payable owner;
-    uint256 public pixelFee;
-    uint256 private _currentTokenID;
-    bytes32[] pixelIds;
-    mapping (bytes32 => Pixel) public pixelMap;
-    mapping (bytes32 => uint256) public pixelExhibit;
-    mapping (uint256 => Exhibit) exhibits;
-    mapping (uint256 => Bid) highestBid;
-    mapping (uint256 => Bid[]) bidHistory;
-    uint256 public maxXPixel;
-    uint256 public maxYPixel;
-    uint256 public maxPixelsPerExhibit;
-    
-    mapping (bytes32 => bool) tempPixelMap;
+    uint16 public pixelFee;
+    uint16 public maxXPixel;
+    uint16 public maxYPixel;
+    uint16 public maxPixelsPerExhibit;
+    uint16 private _currentTokenID;
+    mapping (uint16 => Bid) highestBid;
+    mapping (uint16 => Bid[]) bidHistory;
+    mapping (uint16 => Bounds) bounds;
+    mapping (uint16 => uint8[]) rgbArray;
+    uint16[] allBounds;
 
-    modifier exhibitCreatorOnly(uint256 _id) {
-        require(exhibits[_id].owner == msg.sender, "Creator Only");
+    modifier exhibitCreatorOnly(uint16 _id) {
+        require(ownerOf(_id) == msg.sender, "Creator Only");
         _;
     }
 
-    modifier notExhibitCreator(uint256 _id) {
-        require(exhibits[_id].owner != msg.sender, "Cannot be Creator");
+    modifier notExhibitCreator(uint16 _id) {
+        require(ownerOf(_id) != msg.sender, "Cannot be Creator");
         _;
     }
 
@@ -67,8 +59,29 @@ contract PixelToken is ERC721 {
         _;
     }
 
-    constructor(address payable _owner, uint256 _maxCoords, 
-                uint256 _maxPixels, uint256 _pixelfee) ERC721("PixelToken", "PXT") {
+    modifier isValidLength(uint256 length, Bounds memory _bounds) {
+        require(length == (_bounds.bottomRight.x - _bounds.topLeft.x + 1) * (_bounds.bottomRight.y - _bounds.topLeft.y + 1), "Invalid pixels within bounds");
+        _;
+    }
+
+    modifier isInBounds(Bounds memory _bounds) {
+        require(!validBounds(allBounds, _bounds), "Bounds overlap with another");
+        _;
+    }
+
+    modifier isPixelsValid(uint8[] memory _pixels) {
+        require(_pixels.length <= maxPixelsPerExhibit * 3, "Too many pixels");
+        _;
+    }
+
+    modifier isOnCanvas(Bounds memory _newBounds) {
+        require(_newBounds.topLeft.x <= maxXPixel && _newBounds.topLeft.y <= maxYPixel 
+            && _newBounds.bottomRight.x <= maxXPixel && _newBounds.bottomRight.y <= maxYPixel, "Y or X is too large");
+        _;
+    }
+
+    constructor(address payable _owner, uint16 _maxCoords, 
+                uint16 _maxPixels, uint16 _pixelfee) ERC721("Exhibit", "XBT") {
         owner = _owner;
         maxXPixel = _maxCoords;
         maxYPixel = _maxCoords;
@@ -76,62 +89,51 @@ contract PixelToken is ERC721 {
         pixelFee = _pixelfee;
     }
 
-    function getPixels() public view returns(PixelResponse[] memory) {
-        PixelResponse[] memory _pixels = new PixelResponse[](pixelIds.length);
-        for (uint256 i = 0; i < _pixels.length; i++) {
-            Pixel memory _pixel = pixelMap[pixelIds[i]];
-            Exhibit memory creator = exhibits[pixelExhibit[_pixel.id]];
-            PixelResponse memory resp = PixelResponse({
-                exhibitId: creator.id,
-                owner: creator.owner,
-                pixelId: _pixel.id,
-                hexColor: _pixel.hexColor,
-                x: _pixel.x,
-                y: _pixel.y
+    function getPixels() public view returns(ExhibitResp[] memory) {
+        ExhibitResp[] memory _exhibit = new ExhibitResp[](_currentTokenID);
+        for (uint16 i = 0; i < _currentTokenID; i++) {
+            uint8[] memory _rgbArray = rgbArray[i];
+            Bounds memory _bounds = bounds[i];
+            ExhibitResp memory resp = ExhibitResp({
+                exhibitId: i,
+                owner: ownerOf(i),
+                rgbArray: _rgbArray,
+                bounds: _bounds
             });
-             _pixels[i] = resp;
+             _exhibit[i] = resp;
         }
-        return _pixels;
+        return _exhibit;
     }
 
-    function create(Pixel[] memory _pixels) public payable {
+    function create(uint8[] memory _pixels, Bounds memory _bounds) public payable 
+                isValidLength(uint16((_pixels.length / 3)), _bounds)
+                isInBounds(_bounds)
+                isPixelsValid(_pixels)
+                isOnCanvas(_bounds) {
+        
         require(msg.sender.balance >= msg.value, "Not enough funds");
-        require(_pixels.length <= maxPixelsPerExhibit, "Too many pixels");
-        require(isFullRect(_pixels), "Invalid rectangle");
-        for (uint256 i = 0; i < _pixels.length; i++) {
-            Pixel memory p = _pixels[i];
-            require(p.x <= maxXPixel && p.y <= maxYPixel, "Coordinates are out of bounce");
-            p.id = getHashFromCords(p.x, p.y);
-            require(pixelMap[p.id].id == 0, "Pixel already taken");
-            // Remove from tempPixelMap as it is only
-            // used for full rect validation
-            delete tempPixelMap[p.id];
-            pixelMap[p.id] = p;
-            pixelExhibit[p.id] = _currentTokenID;
-            pixelIds.push(p.id);
-        }
-
-        Exhibit memory exhibit = Exhibit({
-            id: _currentTokenID,
-            owner: msg.sender
-        });
-        exhibits[_currentTokenID] = exhibit;
+        bounds[_currentTokenID] = _bounds;
+        rgbArray[_currentTokenID] = _pixels;
+        allBounds.push(_bounds.topLeft.x);
+        allBounds.push(_bounds.topLeft.y);
+        allBounds.push(_bounds.bottomRight.x);
+        allBounds.push(_bounds.bottomRight.y);
         owner.transfer(msg.value);
         _mint(msg.sender, _currentTokenID);
         _incrementTokenTypeId();
     }
 
-    function changePixels(uint256 _id, Pixel[] memory _pixels) public exhibitCreatorOnly(_id) {
-        for (uint256 i = 0; i < _pixels.length; i++) {
-            Pixel memory _pixel = _pixels[i];
-            require(pixelExhibit[_pixel.id] == _id);
-            Pixel memory map = pixelMap[_pixel.id];
-            map.hexColor = _pixel.hexColor;
-            pixelMap[_pixel.id] = map;
-        } 
+    function changePixels(uint16 _id, uint8[] memory _rgbArray) public exhibitCreatorOnly(_id) {
+        rgbArray[_id] = _rgbArray;
+        Bid memory _highestBid = highestBid[_id];
+        if (_highestBid.fromAddress != address(0)) {
+            _highestBid.fromAddress.transfer(_highestBid.amount);
+            bidHistory[_id].push(_highestBid);
+        }
+        delete highestBid[_id];
     }
 
-    function placeBid(uint256 _id) notExhibitCreator(_id) public payable {
+    function placeBid(uint16 _id) notExhibitCreator(_id) public payable {
         Bid memory prevHighest = highestBid[_id];
         require(msg.sender.balance >= msg.value);
         require(msg.value > 0 && msg.value > prevHighest.amount);
@@ -148,28 +150,27 @@ contract PixelToken is ERC721 {
         highestBid[_id] = newHighestBidder;
     }
 
-    function acceptBid(uint256 _id) exhibitCreatorOnly(_id) public payable {
-        Exhibit memory c = exhibits[_id];
-        c.owner.transfer(highestBid[_id].amount);
-        safeTransferFrom(c.owner, highestBid[_id].fromAddress, _id);
-        c.owner = highestBid[_id].fromAddress;
-        exhibits[_id] = c;
-        bidHistory[_id].push(highestBid[_id]);
+    function acceptBid(uint16 _id) exhibitCreatorOnly(_id) public payable {
+        address payable _prevOwner = payable(ownerOf(_id));
+        Bid memory _highestBid = highestBid[_id];
+        _prevOwner.transfer(_highestBid.amount);
+        safeTransferFrom(_prevOwner, _highestBid.fromAddress, _id);
+        bidHistory[_id].push(_highestBid);
         delete highestBid[_id];
     }
-
-    function getBid(uint256 _id) public view returns(Bid memory) {
+ 
+    function getBid(uint16 _id) public view returns(Bid memory) {
         return highestBid[_id];
     }
 
-    function getBidHistoryForExhibit(uint256 _id) public view returns(Bid[] memory) {
+    function getBidHistoryForExhibit(uint16 _id) public view returns(Bid[] memory) {
         return bidHistory[_id];
     }
 
     function getAllHighestBids() public view returns(HighestBidResponse[] memory) {
         HighestBidResponse[] memory highestBids = new HighestBidResponse[](_currentTokenID);
-        uint256 index = 0;
-        for (uint256 i = 0; i < _currentTokenID; i++) {
+        uint16 index = 0;
+        for (uint16 i = 0; i < _currentTokenID; i++) {
             if (highestBid[i].amount != 0) {
                 Bid memory _bid = highestBid[i];
                 HighestBidResponse memory resp = HighestBidResponse({
@@ -184,65 +185,53 @@ contract PixelToken is ERC721 {
         return highestBids;
     }
 
-    function _incrementTokenTypeId() private  {
+    // Helpers
+    function _incrementTokenTypeId() internal {
         _currentTokenID++;
     }
 
-    function getHashFromCords(uint256 x, uint256 y) internal pure returns (bytes32) {
+    function getHashFromCords(uint16 x, uint16 y) internal pure returns (bytes32) {
         return sha256(abi.encodePacked(x, y));
    }
 
-   function isFullRect(Pixel[] memory _pixels) internal returns(bool) {
-        uint256 xMax = 0;
-        uint256 yMax = 0;
-        uint256 xMin = maxXPixel;
-        uint256 yMin = maxYPixel;
-        
-        for (uint256 i = 0; i < _pixels.length; i++) {
-            Pixel memory _pixel = _pixels[i];
-            if (_pixel.x > xMax) {
-                xMax = _pixel.x;
-            }
-            if (_pixel.y > yMax) {
-                yMax = _pixel.y;
-            }
-            if (_pixel.x < xMin) {
-                xMin = _pixel.x;
-            }
-            if (_pixel.y < yMin) {
-                yMin = _pixel.y;
-            }
-            bytes32 _id = getHashFromCords(_pixel.x, _pixel.y);
-            if (!tempPixelMap[_id]) {
-                tempPixelMap[_id] = true;
-            } else {
-                return false;
-            }
-        }
-        if (_pixels.length < (xMax - xMin + 1) * (yMax - yMin + 1)) {
-            return false;
-        }
-        return true;
-   }
+   function validBounds(uint16[] memory _bounds, Bounds memory _newBounds) internal pure returns(bool) {
+       for (uint256 i = 0; i < _bounds.length; i += 4) {
+           uint16 _topLeftX = _bounds[i];
+           uint16 _topLeftY = _bounds[i+1];
+           uint16 _bottomRightX = _bounds[i+2];
+           uint16 _bottomRightY = _bounds[i+3];
 
+           if (_topLeftX <= _newBounds.topLeft.x 
+                    && _topLeftY <= _newBounds.topLeft.y
+                    && _bottomRightX > _newBounds.topLeft.x
+                    && _bottomRightY > _newBounds.topLeft.y) {
+                return true;
+           } else if (_topLeftX <= _newBounds.bottomRight.x 
+                    && _topLeftY <= _newBounds.bottomRight.y
+                    && _bottomRightX > _newBounds.bottomRight.x
+                    && _bottomRightY > _newBounds.bottomRight.y) {
+                return true;
+           } 
+       }
+       return false;
+   }
 
     // Admin
     function changeContactOwner(address payable _owner) public isContractOwner {
         owner = _owner;
     }
 
-    function changeMaxXYPixel(uint256 _maxX, uint256 _maxY) public isContractOwner {
+    function changeMaxXYPixel(uint16 _maxX, uint16 _maxY) public isContractOwner {
         require(_maxX > maxXPixel && _maxY > maxYPixel);
         maxXPixel = _maxX;
         maxYPixel = _maxY;
     }
 
-    function changePixelFee(uint256 _pixelFee) public isContractOwner {
+    function changePixelFee(uint16 _pixelFee) public isContractOwner {
         pixelFee = _pixelFee;
-        // $0.50 / pixel; Use chainlink to update pixelFee as the price of ETH changes
     }
 
-    function changeMaxPixelsPerExhibit(uint256 _max) public isContractOwner {
+    function changeMaxPixelsPerExhibit(uint16 _max) public isContractOwner {
         maxPixelsPerExhibit = _max;
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ module.exports = function (deployer) {
   deployer.deploy(
     PixelToken,
     "0x4606Ac07453a0eFa44cf5C9e17b17D909d1688D2",
-    9,
+    50,
     6000,
     100000
   )
